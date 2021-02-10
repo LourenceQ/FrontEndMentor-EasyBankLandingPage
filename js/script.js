94160160d0fc3c08b156e5ada556111047c01ad6
lou <lqfmecatronica@gmail.com>
2021-02-10 16:01:04 -0300

    styling

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 const header = document.querySelector('.header');
 const btnHamburguer = document.querySelector('#btnHamburguer');
+const overlay = document.querySelector('.overlay');
 
 
 btnHamburguer.addEventListener('click',function(){
     console.log('click Hamburguer');
 
-    if(header.classList.contains('open')){
+    if(header.classList.contains('open')){// close hamburguer menu
         header.classList.remove('open') ;
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
      }
-     else{
+     else{// open hamburuer menu
          header.classList.add('open');
+         overlay.classList.remove('fade-out');
+         overlay.classList.add('fade-in');
      }    
 });
